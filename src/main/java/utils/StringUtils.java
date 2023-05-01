5f4dedf28fe4bde5db73741bfa3c306d01e20ff7
Kaio Martins <kaio.martins2@aluno.unip.br>
2023-05-01 13:12:55 -0300

    implementacao movida para outra classe

--- src/main/java/utils/StringUtils.java
+++ src/main/java/utils/StringUtils.java
@@ -1,9 +1,21 @@
 package utils;
 
+import java.io.File;
 import java.util.Arrays;
 import java.util.stream.Collectors;
 
 public class StringUtils {
+
+    public static String getExtension(File f) {
+        String ext = null;
+        String s = f.getName();
+        int i = s.lastIndexOf('.');
+
+        if (i > 0 &&  i < s.length() - 1) {
+            ext = s.substring(i+1).toLowerCase();
+        }
+        return ext;
+    }
 
     public static String toPascalCase(String str) {
         return Arrays.stream(str.toLowerCase().split("[\\s-]+"))
