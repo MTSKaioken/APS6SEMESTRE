91dec6529882db70cdaa54aba3fb822e01256267
Kaio Martins <kaio.martins2@aluno.unip.br>
2023-05-01 13:16:18 -0300

    alterando impl para classe anonima, texto mais sugestivo e reposicionando componentes

--- src/main/java/ui/ImageView.java
+++ src/main/java/ui/ImageView.java
@@ -2,10 +2,11 @@ package ui;
 
 import lombok.Getter;
 import lombok.Setter;
-import utils.FileTypeFilter;
-import utils.JFilePicker;
+import utils.StringUtils;
 
 import javax.swing.*;
+import javax.swing.filechooser.FileFilter;
+import javax.swing.filechooser.FileNameExtensionFilter;
 import java.awt.*;
 import java.awt.event.ActionListener;
 import java.io.File;
@@ -17,6 +18,24 @@ public class ImageView extends JFrame {
     private JLabel imagemEntrada = new JLabel();
     private JLabel imagemSaida = new JLabel();
     private JFileChooser files = new JFileChooser();
+    private FileFilter extensoesAceitas = new FileFilter() {
+        @Override
+        public boolean accept(File file) {
+            String extension = StringUtils.getExtension(file);
+            if (extension != null) {
+                if (extension.endsWith("png") || extension.endsWith("jpg")) {
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        @Override
+        public String getDescription() {
+            return null;
+        }
+    };
+
 
     private JFileChooser exploradorSaida = new JFileChooser();
     private JComboBox<String> filtros = new JComboBox<>();
@@ -28,7 +47,6 @@ public class ImageView extends JFrame {
 
     private void setarPropriedadesComponentes() {
         JPanel imagePanel = new JPanel();
-        JFilePicker filePicker = new JFilePicker();
 
         imagePanel.setLayout(null);
         Dimension size = Toolkit.getDefaultToolkit().getScreenSize();
@@ -43,7 +61,7 @@ public class ImageView extends JFrame {
         ImageIcon iconeDefault = new ImageIcon("src/main/resources/no_image.jpg");
         iconeDefault.setImage(iconeDefault.getImage().getScaledInstance(416, 416, Image.SCALE_REPLICATE));
 
-        imagemEntrada.setBounds(((int) size.getWidth() / 2)  - 436, 260, 416, 550);
+        imagemEntrada.setBounds(((int) size.getWidth() / 2) - 436, 260, 416, 550);
         imagemSaida.setBounds(((int) size.getWidth() / 2) + 20, 260, 416, 550);
         imagemSaida.setIcon(iconeDefault);
         imagemEntrada.setIcon(iconeDefault);
@@ -54,28 +72,27 @@ public class ImageView extends JFrame {
         files.setVisible(true);
         files.setBounds(0, 0, (int) size.getWidth(), 300);
 
-        filePicker.setMode(JFilePicker.MODE_OPEN);
-        String[] tiposDefiltros = { "Filtro Preto e Branco", "Filtro Negativo", "Filtro Remover ruido", "Filtro Melhorar contraste", "Filtro Tons de cinza", "Filtro Detectar Bordas" };
+        String[] tiposDefiltros = {"Filtro Preto e Branco", "Filtro Negativo", "Filtro Remover ruido", "Filtro Melhorar contraste", "Filtro Tons de cinza", "Filtro Detectar Bordas"};
+        files.addChoosableFileFilter(new FileNameExtensionFilter("Arquivos de Imagem png", "png"));
+        files.addChoosableFileFilter(new FileNameExtensionFilter("Arquivos de Imagem jpg", "jpg"));
 
-        files.addChoosableFileFilter(new FileTypeFilter("jpg", "jpg Images"));
-        files.addChoosableFileFilter(new FileTypeFilter("png", "PNG Images"));
-
-        filtros.setBounds(((int) size.getWidth() / 2) + 65 , 300, 300, 20);
+        filtros.setBounds(((int) size.getWidth() / 2) - 436, 300, 416, 20);
         filtros.setModel(new DefaultComboBoxModel<>(tiposDefiltros));
 
-        botaoSalvar.setBounds(((int) size.getWidth() / 2)  - 436, 760, 872, 30);
-        botaoSalvar.setText("Salvar Imagem");
+        botaoSalvar.setBounds(((int) size.getWidth() / 2) + 20, 300, 416, 20);
+        botaoSalvar.setText("Exportar Imagem");
         botaoSalvar.setFont(new Font("Dialog", Font.BOLD, 16));
 
         adicionarComponentesParaView(imagePanel);
     }
 
-    public void limparImagens(){
+    public void limparImagens() {
         imagemEntrada.setIcon(new ImageIcon("src/main/resources/no_image.jpg"));
         imagemSaida.setIcon(new ImageIcon("src/main/resources/no_image.jpg"));
     }
 
-    public void exibirMensagem(String mensagem){
+
+    public void exibirMensagem(String mensagem) {
         JOptionPane.showMessageDialog(null, mensagem);
     }
 
@@ -83,7 +100,7 @@ public class ImageView extends JFrame {
         files.addActionListener(actionListener);
     }
 
-    public void observaSalvarArquivo(ActionListener actionListener){
+    public void observaSalvarArquivo(ActionListener actionListener) {
         botaoSalvar.addActionListener(actionListener);
     }
 
